89ba35e56e73a78b7c8cf264cedd9deb2d27af2a
Lìon <idev.aymen@gmail.com>
2019-02-27 18:46:46 -0800

    Add files via upload

---
+++ Libft/Tries/trie_getitem.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trie_getitem.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akharrou <akharrou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/26 20:40:07 by akharrou          #+#    #+#             */
+/*   Updated: 2019/02/27 16:44:22 by akharrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void			*trie_getitem(t_trie *root, char *hash,
+				unsigned int (*char_to_index)(char c))
+{
+	unsigned int index;
+
+	if (root && hash && char_to_index)
+	{
+		if (*hash == '\0')
+			return (root->item);
+		index = char_to_index(*hash);
+		return (trie_getitem(
+			(root->children)[index], hash + 1, char_to_index));
+	}
+	return (NULL);
+}
