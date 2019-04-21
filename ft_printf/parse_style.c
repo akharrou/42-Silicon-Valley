bf11c5eef0611fbb4f117e3547e8d8b3050a3e4d
Aymen Kh <idev.aymen@gmail.com>
2019-04-20 22:06:08 -0700

    Latest Update

--- ft_printf/parse_style.c
+++ ft_printf/parse_style.c
@@ -6,7 +6,7 @@
 /*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 15:47:32 by akharrou          #+#    #+#             */
-/*   Updated: 2019/04/20 21:52:21 by akharrou         ###   ########.fr       */
+/*   Updated: 2019/04/20 22:06:03 by akharrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,23 @@
 **                                 string.
 **
 **    DESCRIPTION
-**         Parses for the 'specifier' field in the formatted string.
+**         Parses for the 'style' field in the formatted string.
 **
-**         We check if the character we are on is part of the (handled)
-**         specifiers. If the specifier is indeed part of them, then it
-**         is returned (as an 8 bit integer); otherwise NONE (-1) is
-**         returned.
-**
-**         The index 'i' is incremented accordingly.
+**         We first check for an opening bracket. If there is one,
+**         we will then look for its matching (closing) bracket.
+**         If it is found we will parse out all the styles specified
+**         in between the two brackets, storing them in a 2D array.
+**         Finally the 2D array will be returned.
 **
 **    RETURN VALUES
-**         Returns the obtained specifier (as an 8 bit integer) or NONE (-1,
-**         macro defined in ft_printf.h) to indicate that no valid specifier
-**         was specified.
+**         If styles we're specified, then a 2D array containing them
+**         will be returned; otherwise NULL is returned.
 */
 
 #include "ft_printf.h"
 
 t_char	**parse_style(const char *format, t_int8 *i)
 {
-	t_int16		j;
 	t_char		**styles;
 	t_char		*closing_bracket;
 
@@ -58,23 +55,13 @@ t_char	**parse_style(const char *format, t_int8 *i)
 		if (closing_bracket)
 		{
 			format = ft_strdup_range(
-				format, (*i) + 1, closing_bracket - (format + (*i) - 1));
+						format,
+						(*i) + 1,
+						closing_bracket - (format + (*i) - 1)
+					);
 			styles = ft_strsplit(format, ' ');
-			if (styles)
-				while (*styles)
-				{
-					j = 0;
-					while (ft_strcmp())
-				}
 		}
 	}
 	free((void *)format);
 	return (styles);
 }
-
-
-
-
-
-// #include "Libft/String/ft_strdup_range.c"
-// #include "Libft/String/ft_strsplit.c"
