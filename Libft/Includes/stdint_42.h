eb3d04c15bd042e1369827b203491d18aefc0797
Aymen Kh <idev.aymen@gmail.com>
2019-04-25 07:53:13 -0700

    Latest Update

--- Libft/Includes/stdint_42.h
+++ Libft/Includes/stdint_42.h
@@ -6,26 +6,37 @@
 /*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 12:03:50 by akharrou          #+#    #+#             */
-/*   Updated: 2019/04/18 18:48:14 by akharrou         ###   ########.fr       */
+/*   Updated: 2019/04/25 07:52:25 by akharrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STDINT_42_H
 # define STDINT_42_H
 
-typedef char				t_bool;
-typedef char				t_char;
+# include <stddef.h>
+# include <stdint.h>
 
-typedef unsigned char		t_uint8;
-typedef unsigned short		t_uint16;
-typedef unsigned long		t_uint32;
-typedef unsigned long long	t_uint64;
-
-typedef signed char			t_int8;
-typedef signed short		t_int16;
-typedef signed long			t_int32;
-typedef signed long long	t_int64;
-
-unsigned int				ft_hash(const char *str);
+typedef union				u_data
+{
+	char					*str_;
+	signed char				char_;
+	signed short			short_;
+	signed int				int_;
+	signed long				long_;
+	signed long long		long_long_;
+	unsigned char			uchar_;
+	unsigned short			ushort_;
+	unsigned int			uint_;
+	unsigned long			ulong_;
+	unsigned long long		ulong_long_;
+	double					double_;
+	long double				long_double_;
+	intmax_t				intmax_;
+	uintmax_t				uintmax_;
+	intptr_t				intptr_;
+	uintptr_t				uintptr_;
+	ptrdiff_t				ptrdiff_;
+	size_t					size_;
+}							t_data;
 
 #endif
